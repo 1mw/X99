e57670674d76281555b86fdb298d0eb632b1c66c
Mark Aldrich <admin@example.com>
2015-11-28 21:11:03 -0500

    Updated g

--- X99/include/g.h
+++ X99/include/g.h
@@ -6,35 +6,27 @@ int g_inputAndExec();
 /*
 G is a programming language for the X99 operating system.
 
+"$" is used for variables.
+
+"#" is used for registers.
+
 "@" is used for operators. Ex:
 	- @+ (num1) :: (num2) :: (result)
 	- @- (num1) :: (num2) :: (result)
 	- @* (num1) :: (num2) :: (result)
 	- @/ (num1) :: (num2) :: (result)
-	!- @init (var)
-	- @storel (var) :: (literal)
-	- @storev (var1) :: (var2)
-	- @storer (var1) :: (register)
-	- @yieldl (literal)
-	- @yieldv (var)
-	- @jumpl (line)
-	- @jumpv (var)
-	- @jumpzl (var) :: (line)
-	- @jumpzv (var) :: (var)
-	- @jumppl (var) :: (line)
-	- @jumppv (var) :: (var)
-	- @jumpnl (var) :: (line)
-	- @jumpnv (var) :: (var)
-	- @jumpeqv (var)
-	- @equall (var) :: (literal) :: (result)
-	- @equalv (var) :: (var) :: (result)
-	- @pushv (var)
-	- @pushl (literal)
+	- @init (var)
+	- @store (var) :: (value)
+	- @yield (value)
+	- @jump (line)
+	- @jumpz (var) :: (line)
+	- @jumpp (var) :: (line)
+	- @jumpn (var) :: (line)
+	- @equal (var) :: (literal) :: (result)
+	- @push (value)
 	- @pop (var)
-	- @printv (var)
-	- @printl (literal)
-	- @printcv (var)
-	- @printcl (literal)
+	- @printv (value)
+	- @printc (value)
 	- @compare (var1) :: (var2)
 ";" is for comments.
 */
